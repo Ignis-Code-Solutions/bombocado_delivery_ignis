07cffc5104e1c328462e2970225db11462983c7d
Jonathan Leao <jonathan.sleao@gmail.com>
2026-07-31 16:35:59 -0300

    chore: criando estrutura inicial do projeto Spring Boot

---
+++ src/main/java/com/generation/bombocado/BombocadoDeliveryApplication.java
@@ -0,0 +1,13 @@
+package com.generation.bombocado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BombocadoDeliveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BombocadoDeliveryApplication.class, args);
+	}
+
+}
